6640c6af36c7fbdbf98c8509de2b1711577a7f64
Ldeng928 <lideng928@gmail.com>
2021-09-30 10:52:39 -0400

    Connect to MongoDB altas

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURL');
+
+const connectDB = async () => {
+    // Re-factored to async/await
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+
+        console.log('MongoDB connected...')
+    } catch {
+        console.error(err.message); 
+        process.exit(1);
+    }
+
+    // mongoose.connect(db, {
+    //     useNewUrlParser: true
+    // })
+    // .then(() => console.log('MongoDB Connected'))
+    // .catch(err => {
+    //     console.error(err.message); 
+    //     process.exit(1);
+    // });
+};
+
+module.exports = connectDB;
