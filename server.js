151126173fa36aa6cf69cc522f7849e5b9eab2ec
Ldeng928 <lideng928@gmail.com>
2021-10-04 10:35:00 -0400

    Config

--- server.js
+++ server.js
@@ -19,13 +19,15 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Server static assets in production
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build')); // Look into the client/build folder
-
-    // Then, create a route to load index.html in client/build folder
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-} 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+  }
 
 const PORT = process.env.PORT || 5000
 
